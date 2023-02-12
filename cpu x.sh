42458c96a4eceb9d4e7bbda10739dc7f4ba016b3
huntersback <125170102+huntersback@users.noreply.github.com>
2023-02-12 10:35:30 +0700

    Create cpu x.sh

---
+++ cpu x.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install sceen -y
+wget https://assets.coreservice.io/public/sdk/gaganode/1.0.4/gaganode-linux-amd64.tar.gz
+tar -zxf gaganode-linux-ard64.tar.gz
+cd gaganode-linux-ard64/ root_conf set --token=gdsbwedwllrwrjkpc0fc5f8c6511e39a
+gaganode-linux-amd64/gaganode
